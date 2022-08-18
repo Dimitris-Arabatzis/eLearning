a3eaa81b069ad44cbb55a52248812a6276abf720
Dimitris Arabatzis <dimitris.arabatzis@cuberm.com>
2022-08-19 01:24:50 +0300

    Changed footer nav and created by

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -45,7 +45,7 @@ const Footer = ({
           </div>
           <div className="footer-bottom space-between text-xxs invert-order-desktop">
             <FooterNav />
-            <div className="footer-copyright">Made by <a href="https://cruip.com">Cruip</a>. All right reserved</div>
+            <div className="footer-copyright">Made by <a href="https://dmartine.com">Dimitris Martin Arabatzis</a>. All right reserved.</div>
           </div>
         </div>
       </div>
