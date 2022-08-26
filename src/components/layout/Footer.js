2a892ebdb18f3fe89dee8651a6326d9c158638d2
Dimitris Arabatzis <dimitris.arabatzis@cuberm.com>
2022-08-26 15:46:31 +0300

    Fix typo

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -45,7 +45,7 @@ const Footer = ({
           </div>
           <div className="footer-bottom space-between text-xxs invert-order-desktop">
             <FooterNav />
-            <div className="footer-copyright">Made by <a href="https://damartine.com">damartine.com</a> and the Lingi team. All right reserved.</div>
+            <div className="footer-copyright">Made by <a href="https://damartine.com">damartine.com</a> and the Lingi team. All rights reserved.</div>
           </div>
         </div>
       </div>
