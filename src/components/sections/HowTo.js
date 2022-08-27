a20df6a301831c65d159cd5ea52433117103bab0
Dimitris Arabatzis <dimitris.arabatzis@cuberm.com>
2022-08-27 13:27:46 +0300

    Add how to changes & colors

--- src/components/sections/HowTo.js
+++ src/components/sections/HowTo.js
@@ -5,6 +5,8 @@ import SectionHeader from './partials/SectionHeader';
 import ButtonGroup from '../elements/ButtonGroup';
 import Button from '../elements/Button';
 import { useState } from 'react';
+import Image from '../elements/Image';
+
 
 const propTypes = {
   ...SectionSplitProps.types
@@ -58,30 +60,6 @@ const HowTo = ({
 
   const[isStudent,setIsStudent] = useState(false);
 
-  var textsStudent = {
-    step1: "Αν θες να παρακολουθήσεις βιντεοσκοπημένα μαθήματα ή να βρεις καθηγητές για ιδιαίτερα.",
-    step1Header: "Επιλέγεις...",
-    step1Comment: "Εύκολη επιλογή",
-    step2: "Τα στοιχεία επικοινωνίας σου.",
-    step2Header: "Συμπληρώνεις...",
-    step2Comment: "Απλά και κατανοητά",
-    step3: "Κάνεις μαθήματα όπου και όποτε θελήσεις",
-    step3Header: "Αντιστοίχηση με καθηγητές",
-    step3Comment: "Γρήγορη διαδικασία",
-  };
-
-  var textsTeacher = {
-    step1: "Αν θέλεις να κάνεις ιδιαίτερα ή αν σε ενδιαφέρει να συνεργαστείς με την ομάδα μας και να μπεις στον νέο κόσμο της ασύγχρονης εκπαίδευσης.",
-    step1Header: "Επιλέγεις...",
-    step1Comment: "Εύκολη επιλογή",
-    step2: "Συμπληρώνεις τα στοιχεία μαζί με το βιογραφικό σου και τα μαθήματα που διδάσκεις.",
-    step2Header: "Συμπληρώνεις...",
-    step2Comment: "Απλά και κατανοητά",
-    step3: "Οι μαθητές βλέπουν το προφίλ σου και επικοινωνούν μαζί σου!",
-    step3Header: "Αντιστοίχηση με μαθητές",
-    step3Comment: "Γρήγορη διαδικασία",
-  };
-
   return (
     <section
       {...props}
@@ -92,82 +70,21 @@ const HowTo = ({
           <SectionHeader data={sectionHeader} className="center-content" />
 
           <ButtonGroup className={"how-to-buttons"}>
-            <Button tag="a" color={isStudent ? "primary" : "dark"} wideMobile onClick={() => setIsStudent(true)}>
+            <Button tag="a" color={isStudent ? "primary how-to-choosen" : "dark"} wideMobile onClick={() => setIsStudent(true)}>
               Μαθητής/τρια
             </Button>
-            <Button tag="a" color={!isStudent ? "primary" : "dark"} wideMobile onClick={() => setIsStudent(false)}>
+            <Button tag="a" color={!isStudent ? "primary how-to-choosen" : "dark"} wideMobile onClick={() => setIsStudent(false)}>
               Καθηγητής/τρια
             </Button> 
           </ButtonGroup>
 
           <div className={splitClasses}>
 
-            <div className="split-item">
-              <div className="split-item-content center-content-mobile reveal-from-left" data-reveal-container=".split-item">
-                <div className="text-xxs text-color-primary fw-600 tt-u mb-8">
-                  { isStudent ? textsStudent.step1Comment : textsTeacher.step1Comment}
-                  </div>
-                <h3 className="mt-0 mb-12">
-                { isStudent ? textsStudent.step1Header : textsTeacher.step1Header}
-                  </h3>
-                <p className="m-0">
-                  { isStudent ? textsStudent.step1 : textsTeacher.step1}
-                  </p>
-              </div>
-              <div className={
-                classNames(
-                  'split-item-image center-content-mobile reveal-from-bottom',
-                  imageFill && 'split-item-image-fill'
-                )}
-                data-reveal-container=".split-item">
-                <h3 className='how-to-step'>Βήμα 1</h3>
-              </div>
-            </div>
-
-            <div className="split-item">
-              <div className="split-item-content center-content-mobile reveal-from-right" data-reveal-container=".split-item">
-                <div className="text-xxs text-color-primary fw-600 tt-u mb-8">
-                { isStudent ? textsStudent.step2Comment : textsTeacher.step2Comment}
-                  </div>
-                <h3 className="mt-0 mb-12">
-                { isStudent ? textsStudent.step2Header : textsTeacher.step2Header}
-                  </h3>
-                <p className="m-0">
-                { isStudent ? textsStudent.step2 : textsTeacher.step2}
-                  </p>
-              </div>
-              <div className={
-                classNames(
-                  'split-item-image center-content-mobile reveal-from-bottom',
-                  imageFill && 'split-item-image-fill'
-                )}
-                data-reveal-container=".split-item">
-                <h3 className='how-to-step'>Βήμα 2</h3>
-              </div>
-            </div>
-
-            <div className="split-item">
-              <div className="split-item-content center-content-mobile reveal-from-left" data-reveal-container=".split-item">
-                <div className="text-xxs text-color-primary fw-600 tt-u mb-8">
-                { isStudent ? textsStudent.step3Comment : textsTeacher.step3Comment}
-                  </div>
-                <h3 className="mt-0 mb-12">
-                { isStudent ? textsStudent.step3Header : textsTeacher.step3Header}
-                  </h3>
-                <p className="m-0">
-                { isStudent ? textsStudent.step3 : textsTeacher.step3}
-                  </p>
-              </div>
-              <div className={
-                classNames(
-                  'split-item-image center-content-mobile reveal-from-bottom',
-                  imageFill && 'split-item-image-fill'
-                )}
-                data-reveal-container=".split-item">
-                <h3 className='how-to-step'>Βήμα 3</h3>
-
-              </div>
-            </div>
+          <Image
+            src={isStudent ? require('./../../assets/images/HowToStudent.png') : require('./../../assets/images/HowToTeacher.png') }
+            alt="Open"
+            
+             />
 
           </div>
         </div>
