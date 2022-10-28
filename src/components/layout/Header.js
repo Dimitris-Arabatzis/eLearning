178833395a0c3a2a5990832930211233654279d7
Dimitris Arabatzis <dimitris.arabatzis@cuberm.com>
2022-10-28 19:42:55 +0300

    Add days to countdown

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,25 +1,25 @@
-import React, { useState, useRef, useEffect } from 'react';
-import PropTypes from 'prop-types';
-import classNames from 'classnames';
-import { Link } from 'react-router-dom';
-import Logo from './partials/Logo';
-import Image from '../elements/Image';
+import React, { useState, useRef, useEffect } from "react";
+import PropTypes from "prop-types";
+import classNames from "classnames";
+import { Link } from "react-router-dom";
+import Logo from "./partials/Logo";
+import Image from "../elements/Image";
 
 const propTypes = {
   navPosition: PropTypes.string,
   hideNav: PropTypes.bool,
   hideSignin: PropTypes.bool,
   bottomOuterDivider: PropTypes.bool,
-  bottomDivider: PropTypes.bool
-}
+  bottomDivider: PropTypes.bool,
+};
 
 const defaultProps = {
-  navPosition: '',
+  navPosition: "",
   hideNav: false,
   hideSignin: false,
   bottomOuterDivider: false,
-  bottomDivider: false
-}
+  bottomDivider: false,
+};
 
 const Header = ({
   className,
@@ -30,7 +30,6 @@ const Header = ({
   bottomDivider,
   ...props
 }) => {
-
   const [isActive, setIsactive] = useState(false);
 
   const nav = useRef(null);
@@ -38,74 +37,81 @@ const Header = ({
 
   useEffect(() => {
     isActive && openMenu();
-    document.addEventListener('keydown', keyPress);
-    document.addEventListener('click', clickOutside);
+    document.addEventListener("keydown", keyPress);
+    document.addEventListener("click", clickOutside);
     return () => {
-      document.removeEventListener('keydown', keyPress);
-      document.removeEventListener('click', clickOutside);
+      document.removeEventListener("keydown", keyPress);
+      document.removeEventListener("click", clickOutside);
       closeMenu();
     };
-  });  
+  });
 
   const openMenu = () => {
-    document.body.classList.add('off-nav-is-active');
-    nav.current.style.maxHeight = nav.current.scrollHeight + 'px';
+    document.body.classList.add("off-nav-is-active");
+    nav.current.style.maxHeight = nav.current.scrollHeight + "px";
     setIsactive(true);
-  }
+  };
 
   const closeMenu = () => {
-    document.body.classList.remove('off-nav-is-active');
+    document.body.classList.remove("off-nav-is-active");
     nav.current && (nav.current.style.maxHeight = null);
     setIsactive(false);
-  }
+  };
 
   const keyPress = (e) => {
     isActive && e.keyCode === 27 && closeMenu();
-  }
+  };
 
   const clickOutside = (e) => {
-    if (!nav.current) return
-    if (!isActive || nav.current.contains(e.target) || e.target === hamburger.current) return;
+    if (!nav.current) return;
+    if (
+      !isActive ||
+      nav.current.contains(e.target) ||
+      e.target === hamburger.current
+    )
+      return;
     closeMenu();
-  }  
+  };
 
   const classes = classNames(
-    'site-header',
-    bottomOuterDivider && 'has-bottom-divider',
+    "site-header",
+    bottomOuterDivider && "has-bottom-divider",
     className
   );
 
-  let date_1 = new Date('12/28/2022');
+  let date_1 = new Date("01/15/2023");
   let date_2 = new Date();
-  const days = (date_1, date_2) =>{
+  const days = (date_1, date_2) => {
     let difference = date_1.getTime() - date_2.getTime();
     let TotalDays = Math.ceil(difference / (1000 * 3600 * 24));
     return TotalDays;
-  }
+  };
 
   return (
-    <header
-      {...props}
-      className={classes}
-    >
+    <header {...props} className={classes}>
       <div className="container">
-        <div className={
-          classNames(
-            'site-header-inner',
-            bottomDivider && 'has-bottom-divider'
-          )}>
+        <div
+          className={classNames(
+            "site-header-inner",
+            bottomDivider && "has-bottom-divider"
+          )}
+        >
           <Logo />
           <div className="launching">
-          <Image className="reveal-from-bottom" data-reveal-delay="600"
-                      src={require('./../../assets/images/rocket.svg')}
-                      alt="About us icon explanation"
-                      width={40}
-                      />
-          
-            <p >Live <br></br>in <span>{days(date_1,date_2)}</span><br></br> days!</p>
+            <Image
+              className="reveal-from-bottom"
+              data-reveal-delay="600"
+              src={require("./../../assets/images/rocket.svg")}
+              alt="About us icon explanation"
+              width={40}
+            />
 
+            <p>
+              Live <br></br>in <span>{days(date_1, date_2)}</span>
+              <br></br> days!
+            </p>
           </div>
-          {!hideNav &&
+          {!hideNav && (
             <>
               <button
                 ref={hamburger}
@@ -119,11 +125,8 @@ const Header = ({
               </button>
               <nav
                 ref={nav}
-                className={
-                  classNames(
-                    'header-nav',
-                    isActive && 'is-active'
-                  )}>
+                className={classNames("header-nav", isActive && "is-active")}
+              >
                 <div className="header-nav-inner">
                   {/* <ul className={
                     classNames(
@@ -134,22 +137,28 @@ const Header = ({
                       <Link to="#0" onClick={closeMenu}>Documentation</Link>
                     </li>
                   </ul> */}
-                  {!hideSignin &&
-                    <ul
-                      className="list-reset header-nav-right"
-                    >
+                  {!hideSignin && (
+                    <ul className="list-reset header-nav-right">
                       <li>
-                        <Link to="/Contact" className="button button-primary button-wide-mobile button-sm" onClick={closeMenu}>Επικοινωνία</Link>
+                        <Link
+                          to="/Contact"
+                          className="button button-primary button-wide-mobile button-sm"
+                          onClick={closeMenu}
+                        >
+                          Επικοινωνία
+                        </Link>
                       </li>
-                    </ul>}
+                    </ul>
+                  )}
                 </div>
               </nav>
-            </>}
+            </>
+          )}
         </div>
       </div>
     </header>
   );
-}
+};
 
 Header.propTypes = propTypes;
 Header.defaultProps = defaultProps;
